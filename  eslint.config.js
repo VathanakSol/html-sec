b143cfae14bc6fa2a9d442ca1526d3087e7f8663
VathanakSol <vathanaksol1605@gmail.com>
2024-12-08 18:01:37 +0700

    modify file

---  eslint.config.js
+++  eslint.config.js
@@ -1,11 +1,15 @@
-// .eslintrc.js
+const js = require("@eslint/js");
+const jsdoc = require("eslint-plugin-jsdoc");
 
-module.exports = {
-    // ...other config
-    plugins: ["jsdoc"],
+module.exports = [
+  js.configs.recommended,
+  {
+    plugins: {
+      jsdoc: jsdoc
+    },
     rules: {
-        "jsdoc/require-description": "error",
-        "jsdoc/check-values": "error"
+      "jsdoc/require-description": "error",
+      "jsdoc/check-values": "error"
     }
-    // ...other config
-};
+  }
+];
