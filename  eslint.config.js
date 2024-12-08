04d5481479f85684e5a2024aa992f4361cf71c2d
VathanakSol <vathanaksol1605@gmail.com>
2024-12-08 17:51:22 +0700

    add new

---
+++  eslint.config.js
@@ -0,0 +1,9 @@
+export default [
+    {
+        files: ["**/*.js", "**/*.ts"], // Specify which files to lint
+        rules: {
+            semi: "error", // Example rule
+            "prefer-const": "error",
+        },
+    },
+];
